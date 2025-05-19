e9f218432e04579fc6d76994ea7d4eb189d9d5bb
imangkn <imangukelani@gmail.com>
2025-05-19 18:26:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace algorithm404
     {
         static void Main(string[] args)
         {
-            node node1 = new node("iman",135322,1);
+            node node1 = new node("iman", 135322, 1);
             tree tree1 = new tree(node1);
-            
+
         }
     }
     class node
@@ -59,20 +59,20 @@ namespace algorithm404
         public static void printinorder(node rishe1)
         {
             if (rishe1 != null)
-            printinorder(rishe1.left);
-            Console.WriteLine("name:"+ rishe1.work ,"id:"+ rishe1.id,"priority:"+ rishe1.priority);
-            printinorder (rishe1.right);
+                printinorder(rishe1.left);
+            Console.WriteLine("name:" + rishe1.work, "id:" + rishe1.id, "priority:" + rishe1.priority);
+            printinorder(rishe1.right);
         }
-        public  node searchbst(node vorudi ,int idgere)
+        public static node searchbst(node vorudi, int idgere)
         {
-            if (vorudi == null || idgere == rishe.id)
+            if (vorudi == null || idgere == vorudi.id)
                 return vorudi;
-            if(idgere < vorudi.id)
+            if (idgere < vorudi.id)
                 return searchbst(vorudi.left, idgere);
             else
                 return searchbst(vorudi.right, idgere);
         }
-        public void transplant (node ghabli , node jadid)
+        public void transplant(node ghabli, node jadid)
         {
             if (ghabli.parent == null)  //shart rishe budan gere
                 rishe = jadid;
@@ -85,24 +85,24 @@ namespace algorithm404
         }
         public static node Minimum(node vorudi)
         {
-            while (vorudi.left != null) 
+            while (vorudi.left != null)
                 vorudi = vorudi.left;
             return vorudi;
         }
         public void deletenodebst(int idgere)
         {
-            node hadaf = searchbst(rishe,idgere);
+            node hadaf = searchbst(rishe, idgere);
             if (hadaf.left == null)         // shart farzand rast
-                transplant(hadaf,hadaf.right);
+                transplant(hadaf, hadaf.right);
             else if (hadaf.right == null)   // shart farzand chap
-                transplant(hadaf , hadaf.left);
+                transplant(hadaf, hadaf.left);
 
             else
             {
                 node janeshin = Minimum(hadaf.right);
                 if (janeshin.parent != hadaf)
                 {
-                    transplant (janeshin,janeshin.right);
+                    transplant(janeshin, janeshin.right);
                     janeshin.right = hadaf.right;
                     janeshin.right.parent = janeshin;
                 }
@@ -125,14 +125,13 @@ namespace algorithm404
         }
         public void maxheapify(int vorudi)
         {
-            int heapsize = heap.Count;
             node chap = heap[vorudi].left;
             node rast = heap[vorudi].right;
             int largest = 0;
-            if (chap.priority <= heapsize && heap[chap.priority].priority > heap[largest].priority )
-                largest=chap.priority;
-            if (rast.priority <= heapsize && heap[rast.priority].priority > heap[largest].priority)
-                largest =rast.priority;
+            if (heap[chap.priority].priority > heap[largest].priority)
+                largest = chap.priority;
+            if (heap[rast.priority].priority > heap[largest].priority)
+                largest = rast.priority;
             if (largest != vorudi)
             {
                 node movaghat = heap[vorudi];
@@ -143,23 +142,63 @@ namespace algorithm404
         }
         public void buildmaxheap()
         {
-            int heapsize = heap.Count;
             for (int i = heap.Count / 2; i > 2; i--)
                 maxheapify(i);
         }
         public void heapsort()
         {
-            int heapsize = heap.Count;
             buildmaxheap();
-            for (int i = heap.Count ;i>2 ;i--)
+            for (int i = heap.Count; i > 2; i--)
             {
-                int movaghat = heap[0].priority;
+                node movaghat = heap[0];
                 heap[0] = heap[i];
-                heap[i].priority =movaghat;
-                heapsize = heapsize - 1;
-                maxheapify(1);
+                heap.RemoveAt(heap.Count);
+                maxheapify(0);
             }
         }
+        public void inserttomaxheap(int meghdarprio, int meghdarid)
+        {
+            heap[heap.Count + 1].priority = meghdarprio;
+            heap[heap.Count + 1].id = meghdarid;
+            maxheapify(heap[heap.Count + 1].priority);
+        }
+        public node deletemaxheap()
+        {
 
+            if (heap.Count < 1)
+            {
+                Console.WriteLine("heap khali mibashad");
+                return null;
+            }
+            node max = heap[0];
+            heap[0] = heap[heap.Count];
+            heap.RemoveAt(heap.Count);
+            maxheapify(heap[0].priority);
+            return max;
+        }
+        public void printmaxheap()
+        {
+            for (int i = 0; i < heap.Count; i++)
+            {
+                Console.WriteLine("priority;"+heap[i].priority +"  id:" + heap[i].id+"  work:"+ heap[i].work);
+            }
+        }
+        public void increasepriority(int meghdarid , int priojadid)
+        {
+            for (int i = 0; i < heap.Count; i++)
+            {
+                if (heap[i].id == meghdarid)
+                {
+                    if (priojadid < heap[i].priority)
+                    {
+                        Console.WriteLine("priority jadid kamtar az ghbali mibashad");
+                        return;
+                    }
+                    heap[i].priority = priojadid;
+                    maxheapify (heap[i].priority);
+                }
+            }
+            Console.WriteLine("id peyda nashod");
+        }
     }
 }
